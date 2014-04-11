af27f1ad571c5fd4b6782f471250fbb8ecc2385c
janzaba <janekzaba@gmail.com>
2014-04-11 15:58:26 +0000

    Dostosowanie panelu admina

--- news/admin.py
+++ news/admin.py
@@ -4,5 +4,13 @@ from django.contrib import admin
 
 from POVRay.news.models import *
 
-admin.site.register(Category)
-admin.site.register(News)
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ('name', 'icon')
+    prepopulated_fields = {'slug': ('name',)}
+
+class NewsAdmin(admin.ModelAdmin):
+    list_display = ('title', 'posted_date')
+    prepopulated_fields = {'slug': ('title',)}
+
+admin.site.register(Category, CategoryAdmin)
+admin.site.register(News, NewsAdmin)

1bf2c68fce62932c6f8a1577452f79533fa7403b
janzaba <janekzaba@gmail.com>
2014-04-24 09:38:37 +0000

    Usunięcie renders - będzie tworzone od nowa

--- renders/models.py
+++
@@ -1,9 +0,0 @@
-# -*- coding: utf-8 -*-
-from django.db import models
-from django.contrib.auth.models import User
-
-class Renders(models.Model):
-    user = models.ForeignKey(User, verbose_name='Użytkownik')
-    created_date = models.DateTimeField('Data dodania', auto_now_add=True)
-    finished_date = models.DateTimeField('Data wykonania renderu')
-    script = models.TextField('Skrypt sceny')
--- settings.py
+++ settings.py
@@ -124,7 +124,6 @@ INSTALLED_APPS = (
     # Uncomment the next line to enable admin documentation:
     # 'django.contrib.admindocs',
     'POVRay',
-    'POVRay.renders',
 )
 
 # A sample logging configuration. The only tangible logging
